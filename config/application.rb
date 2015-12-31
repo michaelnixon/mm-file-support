5d47c0a8130b4fe609ffdf405c221dfc5f138e3e
subirranjan1 <subir.ranjan1@gmail.com>
2015-12-30 23:54:17 -0800

    Test Deployment V4: Added Asset Precompile for js and css

--- config/application.rb
+++ config/application.rb
@@ -23,8 +23,8 @@ module FileSupportMovingstories
     ActsAsTaggableOn.remove_unused_tags = true
     config.assets.paths << Rails.root.join("app", "assets", "fonts")
     config.assets.precompile += %w(*.png *.jpg *.jpeg *.gif)
-    # config.assets.precompile += [ 'appviews.css', 'cssanimations.css', 'dashboards.css', 'forms.css', 'gallery.css', 'graphs.css', 'mailbox.css', 'miscellaneous.css', 'pages.css', 'tables.css', 'uielements.css', 'widgets.css' ]
-    # config.assets.precompile += [ 'appviews.js', 'cssanimations.js','custom.js','dashboards.js','data_tracks.js','forms.js', 'gallery.js', 'graphs.js','inspinia.js' ,'mailbox.js', 'miscellaneous.js', 'pages.js', 'tables.js', 'uielements.js', 'widgets.js', ]
+    config.assets.precompile += [ 'appviews.css', 'cssanimations.css', 'dashboards.css', 'data_tracks.css','forms.css', 'gallery.css', 'graphs.css', 'mailbox.css', 'miscellaneous.css', 'pages.css','projects.css', 'sessions.css','tables.css', 'uielements.css', 'widgets.css' ]
+    config.assets.precompile += [ 'appviews.js', 'cssanimations.js','custom.js','dashboards.js','data_tracks.js','forms.js', 'gallery.js', 'graphs.js','inspinia.js' ,'mailbox.js', 'miscellaneous.js', 'pages.js', 'projects.js','tables.js', 'uielements.js', 'widgets.js', ]
     config.exceptions_app = self.routes
   end
 end
