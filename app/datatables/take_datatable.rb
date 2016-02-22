4911e09da7f0202af83ab31b1bc51c31d2e8f56a
Subir <subir.ranjan1@gmail.com>
2016-02-22 00:55:25 -0800

    playing around width

--- app/datatables/take_datatable.rb
+++ app/datatables/take_datatable.rb
@@ -22,7 +22,7 @@ class TakeDatatable < AjaxDatatablesRails::Base
         # comma separated list of the values for each cell of a table row
         # example: record.attribute,
         #link_to(truncate(record.name, length: 40), take_path(record),class:"underlined")
-        link_to(record.name, take_path(record),class:"underlined"), record.movement_group.try(:name), record.movers.map{|mover| link_to(mover.name, mover_path(mover),class:"underlined") }.join(", ")
+        link_to(record.name, take_path(record),class:"underlined"),  record.movement_group.try(:name), record.movement_group.try(:description),record.movers.map{|mover| link_to(mover.name, mover_path(mover),class:"underlined") }.join(", ")
         #record.sensor_types.map{|type| link_to(type.name, sensor_type_path(type),class:"underlined") }.join(", ")
         #record.movement_group_url,
         #, record.take.try(:name), record.sensor_types.map{|type| link_to(type.name, sensor_type_path(type),class:"underlined") }.join(", "), record.movers.map{|mover| link_to(mover.name, mover_path(mover),class:"underlined") }.join(", "),
