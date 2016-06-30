56077f994f5acba4f53bf470ccc38d299f6e9ae7
Subir <subir.ranjan1@gmail.com>
2016-06-30 00:16:20 -0700

    fixed data_tracks load time

--- app/datatables/data_track_datatable.rb
+++ app/datatables/data_track_datatable.rb
@@ -1,14 +1,14 @@
 class DataTrackDatatable < AjaxDatatablesRails::Base
   include AjaxDatatablesRails::Extensions::Kaminari
-  def_delegators :@view, :link_to, :sensor_type_path, :mover_path, :data_track_path, :truncate
+  def_delegators :@view, :link_to,:data_track_path, :truncate
   def sortable_columns
     # Declare strings in this format: ModelName.column_name
-    @sortable_columns ||= ['DataTrack.name', 'Take.name', 'SensorType.name', 'Mover.name', 'DataTrack.technician', 'DataTrack.recorded_on']
+    @sortable_columns ||= ['DataTrack.name','DataTrack.description', 'Take.name','Take.description']
   end
   #name, take, project, sensors, movers, technician, recording data, tags
   def searchable_columns
     # Declare strings in this format: ModelName.column_name
-    @searchable_columns ||= ['DataTrack.name', 'Take.name', 'SensorType.name', 'Mover.name', 'DataTrack.technician', 'DataTrack.recorded_on']
+    @searchable_columns ||= ['DataTrack.name','DataTrack.description', 'Take.name','Take.description']
   end
 # project.name, taglist
   private
@@ -18,15 +18,14 @@ class DataTrackDatatable < AjaxDatatablesRails::Base
       [
         # comma separated list of the values for each cell of a table row
         # example: record.attribute,
-        link_to(truncate(record.name, length: 40), data_track_path(record),class:"underlined"), record.take.try(:name), record.sensor_types.map{|type| link_to(type.name, sensor_type_path(type),class:"underlined") }.join(", "), record.movers.map{|mover| link_to(mover.name, mover_path(mover),class:"underlined") }.join(", "),
-          record.technician, record.recorded_on, record.tag_list        
+        link_to(truncate(record.name, length: 40), data_track_path(record),class:"underlined"), record.description, record.take.try(:name), record.take.try(:description)      
       ]
     end
   end
 
   def get_raw_records
     # update later to include filtering
-    @data_tracks = DataTrack.where(public:true).includes(:take, :sensor_types, :movers).references(:take, :sensor_types, :movers).distinct
+    @data_tracks = DataTrack.where(public:true).includes(:take).references(:take).distinct
     # if @current_user
     #   @data_tracks.select! { |data_track| data_track.public? or data_track.is_accessible_by?(@current_user)  }
     # else
